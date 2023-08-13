d9aebff94ee69ca2be013b8002d3536697511de2
Neiron07 <neiron@vvv.cash>
2023-08-13 17:11:21 +0600

    banner && background

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import heroMain from "../assets/img/hero-main.png";
+import heroMain from "../assets/img/banner.png";
 
 export default function Main() {
   return (
@@ -8,14 +8,14 @@ export default function Main() {
         <div className="stained-text">
           <h1 className="hero-heading">BILIM JARYS</h1>
           <p>
-            Наша задача - сделать практическую часть вашего развития более увлекательной как в играх
+          Примите участие в захватывающих призовых онлайн турнирах по школьным предметам на платформе BILIM JARYS. Соревнуйтесь с учениками из всей страны, докажите свои знания, выигрывайте призы.
           </p>
           <p>Зарегистрируйся сегодня и получи - 1000 очков рейтинга!</p>
           <Link to="/tournaments">
             <button className="join">Исследовать</button>
           </Link>
         </div>
-        <img src={heroMain} alt="hero-main" />
+        <img src={heroMain} alt="hero-main" style={{ maxWidth: "50%", marginLeft: "auto" }}/>
       </div>
     </section>
   );
