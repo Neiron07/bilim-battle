1444743c646e73b0dd02f4d352efe3c6e0bd99e2
neiron <neiron@vvv.cash>
2023-09-03 20:03:14 +0600

    fix img for seo

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -8,11 +8,7 @@ function SEO() {
                 <meta property="og:title" content="Bilim Jarys" />
                 <meta property="og:description" content="Онлайн платформа для призовых турниров по школьным предметам - Bilim Jarys." />
                 <meta name="keywords" content="Bilim Jarys, турниры по школьным предметам, онлайн образование, призовые соревнования" />
-                <meta property="og:image" itemprop="image" content="https://i.imgur.com/nDNowwr.png" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image:width" content="300" />
-                <meta property="og:image:height" content="300" />
-                <meta property="og:url" content="https://bilimjarys.vercel.app/" />
+                <meta property="og:image" itemprop="image" content="https://imgur.com/cHmqJY1.png" />
             </Helmet>
         </div>
     );
