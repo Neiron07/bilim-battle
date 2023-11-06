a00d1c6ac5bcb650f663a8b13ab54937badadd7e
neiron <neiron@vvv.cash>
2023-11-06 10:41:08 +0600

    carousel

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -83,7 +83,7 @@ export default function Shop() {
     return (
         <section style={{ paddingTop: 15 }}>
             <div className="container">
-            <Carousel autoPlay infiniteLoop dynamicHeight interval={6000} showArrows={false} showStatus={false} onClickItem={() => window.location.href = 'https://bilimjarys.kz/tournaments'}>
+            <Carousel showThumbs={false} autoPlay infiniteLoop dynamicHeight interval={6000} showArrows={false} showStatus={false} onClickItem={() => window.location.href = 'https://bilimjarys.kz/tournaments'}>
                         <div>
                             <img src='https://wmpics.space/di-9RNL.png' alt="banner-1" style={{ borderRadius: '20px' }} />
                         </div>
